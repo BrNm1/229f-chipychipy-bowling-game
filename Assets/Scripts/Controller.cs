29be17d4ec93e10802ca35627ee7da2d9720613c
BrNm1 <Burit.noim@bumail.net>
2024-03-23 20:30:23 +0700

    .

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,7 +7,8 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Input.GetAxisRaw("Horizontal");
+        Input.GetAxisRaw("Vertical");
     }
 
     // Update is called once per frame
