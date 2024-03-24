c459222b14f5c2169a6c3a64da8048f4258862de
BrNm1 <Burit.noim@bumail.net>
2024-03-24 21:38:07 +0700

    as

---
+++ Assets/Scripts/SceneManeger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManeger : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Button clicked!");
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game"); 
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu"); // โหลดซีนหน้าเมนูตอนคลิกปุ่มกลับไปหน้าเมนู
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credit");   //หน้าเครดิต
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit button clicked!");
+        Application.Quit(); 
+    }
+}
